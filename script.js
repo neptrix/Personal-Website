22bde26392991d78cd62895766eedadab6b44be4
Nep trix <smzone07@gmail.com>
2023-12-13 00:00:31 +0545

    k ho k ho
    hait

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-let words = document.querySelectorAll(".word");
+document.addEventListener("DOMContentLoaded", function() {
+    let words = document.querySelectorAll(".word");
 
-words.forEach((word) => {
-    let letters = word.textContent.split("");
-    word.textContent = "";
-    letters.forEach((letter) => {
-        let span = document.createElement("span");
-        span.textContent = letter;
-        span.className = "letter";
-        word.append(span);
+    words.forEach((word) => {
+        let letters = word.textContent.split("");
+        word.textContent = "";
+        letters.forEach((letter) => {
+            let span = document.createElement("span");
+            span.textContent = letter;
+            span.className = "letter";
+            word.append(span);
+        });
     });
+
+    let currentWordIndex = 0;
+    let maxWordIndex = words.length - 1;
+    words[currentWordIndex].style.opacity = "1";
 });
 
-let currentWordIndex = 0;
-let maxWordIndex = words.length -1;
-words[currentWordIndex].style.opacity = "1";
 
 
 let changeText = ()=>{
